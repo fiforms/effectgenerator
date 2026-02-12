980e2ea6b95509e7a8881985533f20afd7a98065
Daniel McFeeters <fiforms@gmail.com>
2026-02-12 00:28:45 -0500

    Increased version number

--- effect_generator.h
+++ effect_generator.h
@@ -20,7 +20,7 @@
 
 // Program version. Can be overridden at compile time with -DEFFECTGENERATOR_VERSION="\"x.y.z\""
 #ifndef EFFECTGENERATOR_VERSION
-#define EFFECTGENERATOR_VERSION "0.1.4"
+#define EFFECTGENERATOR_VERSION "0.1.5"
 #endif
 
 inline const char* getEffectGeneratorVersion() {
